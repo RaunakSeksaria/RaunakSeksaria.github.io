81c7fff1f3b2afda17c9e2df11154fabee494e03
Raunak Seksaria <143739997+RaunakSeksaria@users.noreply.github.com>
2024-10-23 02:39:08 +0530

    Made responsive and used Tailwind
    This uses the standalone Tailwind CLI and can be invoked using

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'lato': ['Lato', 'sans-serif'],
+        'roboto-mono': ['Roboto Mono', 'monospace'],
+        'courier-new': ['Courier New', 'Courier', 'monospace'],
+        'dancing-script': ['Dancing Script', 'cursive'],
+      },},
+  },
+  plugins: [],
+}
+
