c7d21f3eb4e48b48761d38955362baaa9b042ff0
Raunak Seksaria <143739997+RaunakSeksaria@users.noreply.github.com>
2025-02-27 12:01:19 +0530

    unupdated-info-mistake

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
             <div className="w-full md:w-2/3 p-4 md:p-10">
                 <h2 className="w-full font-dancing-script text-2xl md:text-4xl mb-4 text-center md:text-left">About Me</h2>
                 <span id="descriptionAbout" className="relative text-center md:text-left">
-                    A fresher at IIIT-Hyderabad, I am interested in the fields of Math and Computer
+                    A sophomore at IIIT-Hyderabad, I am interested in the fields of Math and Computer
                     Science. I am also a tech geek, programmer, and front end developer. A cinephile, I dream to someday direct
                     a short film of my own. I am also into photography and public speaking.
                 </span><br />
